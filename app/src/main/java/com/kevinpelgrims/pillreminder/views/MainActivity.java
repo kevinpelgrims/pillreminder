1e3f68d54fb3aa933a98a7994897a235319d4a75
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2014-12-27 17:13:02 +0100

    Show/hide and handle back button in toolbar

--- app/src/main/java/com/kevinpelgrims/pillreminder/views/MainActivity.java
+++ app/src/main/java/com/kevinpelgrims/pillreminder/views/MainActivity.java
@@ -1,15 +1,17 @@
 package com.kevinpelgrims.pillreminder.views;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 
 import com.kevinpelgrims.pillreminder.R;
 
-public class MainActivity extends PRActivity {
+public class MainActivity extends PRActivity implements FragmentManager.OnBackStackChangedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, MainFragment.newInstance())
@@ -20,14 +22,27 @@ public class MainActivity extends PRActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                getSupportFragmentManager().popBackStackImmediate();
+                return true;
             case R.id.action_add:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.container, AddReminderFragment.newInstance())
                         .addToBackStack("addReminder")
                         .commit();
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                return true;
+            case R.id.action_save:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        if (getSupportFragmentManager().findFragmentById(R.id.container) instanceof MainFragment) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
     }
 }
