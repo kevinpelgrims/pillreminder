6e89941b32a7b4d8f26479a7b790d732d29ca17d
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2014-12-29 20:00:21 +0100

    Add constants
    These can be customized per build type.

---
+++ app/src/main/java/com/kevinpelgrims/pillreminder/Constants.java
@@ -0,0 +1,9 @@
+package com.kevinpelgrims.pillreminder;
+
+public class Constants {
+    public static final String ENVIRONMENT_DEBUG = "debug";
+    public static final String ENVIRONMENT_RELEASE = "release";
+
+    public static final String ENVIRONMENT = ENVIRONMENT_DEBUG;
+    public static final String API_URL = "http://10.0.2.2:8080/_ah/api/";
+}
