01d546bc8bd8dbb1e2a8cb84b7afbfbdcf833da6
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2015-01-04 15:46:30 +0100

    Add beta build type, using production API

---
+++ app/src/beta/java/com/kevinpelgrims/pillreminder/BuildValues.java
@@ -0,0 +1,6 @@
+package com.kevinpelgrims.pillreminder;
+
+public class BuildValues {
+    public static final String ENVIRONMENT = Constants.ENVIRONMENT_DEBUG;
+    public static final String API_URL = "https://pillreminder-lha.appspot.com/_ah/api/";
+}
